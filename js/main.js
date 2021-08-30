9e3506da78a0c3e7d822fe50c51fac9721316465
lmnhsnl <hesenlileman2015@mail.ru>
2021-08-30 23:48:38 +0400

    Build and style navigation bar.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
 
 // Setting Feature icon to be in the middle of Feature block
 var mediaQuery = window.matchMedia("(max-width: 767px)");
-const featureWidth = document.querySelector(".feature").clientWidth / 2 - 37.5;
-if (mediaQuery.matches)
-  document.querySelector(".feature__icon").style.left = `${featureWidth}px`;
+//const featureWidth = document.querySelector(".feature").clientWidth / 2 - 37.5;
+//if (mediaQuery.matches)
+//document.querySelector(".feature__icon").style.left = `${featureWidth}px`;
+
+//To make Navbar block collapsable
+//const toggleButton = document.querySelector(".nav__toggler");
+//toggleButton.addEventListener("click", function () {
+//  document.querySelector(".list__navbar").classList.toggle("hidden");
+//});
+
+//if (!mediaQuery.matches) {
+// document.querySelector(".list__navbar").classList.remove("hidden");
+//}
