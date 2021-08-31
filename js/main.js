2688294423d45066ee224a08d83332d20a71181f
lmnhsnl <hesenlileman2015@mail.ru>
2021-08-31 20:58:23 +0400

    Build and sytle features block.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 
 // Setting Feature icon to be in the middle of Feature block
 var mediaQuery = window.matchMedia("(max-width: 767px)");
-//const featureWidth = document.querySelector(".feature").clientWidth / 2 - 37.5;
-//if (mediaQuery.matches)
-//document.querySelector(".feature__icon").style.left = `${featureWidth}px`;
+const featureWidthIcon =
+  document.querySelector(".feature").clientWidth / 2 - 37.5;
+const icon = document.querySelectorAll(".feature__icon");
+for (let i = 0; i < icon.length; i++) {
+  if (mediaQuery.matches) icon[i].style.left = `${featureWidthIcon}px`;
+}
 
 //To make Navbar block collapsable
 //const toggleButton = document.querySelector(".nav__toggler");
